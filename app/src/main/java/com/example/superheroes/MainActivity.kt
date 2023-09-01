f0d851d6f6b793ba5237c536d7821741f250efcd
ginger cea <gingercea@gmail.com>
2023-09-01 02:20:42 -0400

    se crea data, se importa R

--- app/src/main/java/com/example/superheroes/MainActivity.kt
+++ app/src/main/java/com/example/superheroes/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.superheroes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.superheroescomics.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
